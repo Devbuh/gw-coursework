0cd5c52d865b27e5cab5b366d96b8e115d17a8c1
Devbuh <D.Dhaliwal-23@student.lboro.ac.uk>
2024-03-11 11:51:01 +0000

    Initial Commit

---
+++ gw coursework/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  gw coursework
+//
+//  Created by (s) Devraj Dhaliwal on 11/03/2024.
+//
+
+import Foundation
+
+let SIZE = 200
+let LOSS = 0.2
+let LOSS_LAYER = 180
+
+func main (){
+    let ez : Double = Double(SIZE)
+    let hy = SIZE - 1
+    let ceze = SIZE
+    let cezh = SIZE
+    let chyh = SIZE - 1
+    let imp0 = 377.0
+    let qtime : Int
+    let maxTime = 450
+    let mm : Double
+    
+    //electric field
+    
+    //magnetic field
+    
+    //electric field coefficients
+    
+    //mgnetic field coefficicents
+    
+    //dt timestep
+
+    
+    
+    
+}
+
+
+func ALLOC( _PNTR : Double,   _NUM : Double,  _TYPE : Double) {
+}
